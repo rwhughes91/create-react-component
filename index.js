d61ff7325e6302167467cd92af1b4bace769b000
Robbie Hughes <rwhughes91@gmail.com>
2020-03-31 23:19:41 -0700

    Added file creation

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const process = require('process');
+const fs = require('fs');
+const path = require('path');
+
+// take in name from command line argument
+const [, , name, location] = process.argv;
+
+const componentPath = path.resolve(path.join(location, name));
+const JsFile = path.join(componentPath, `${name}.js`);
+const CssFile = path.join(componentPath, `${name}.css`);
+
+if (!fs.existsSync(componentPath)) {
+  fs.mkdirSync(componentPath, { recursive: true });
+}
+
+const jsTemplate = `import React from 'react';
+
+import classes from './${name}.module.css'
+
+const ${name} = (props) => {
+  return '';
+};
+
+export default ${name};`;
+
+const cssTemplate = `${name} {}`;
+
+fs.writeFileSync(JsFile, jsTemplate);
+fs.writeFileSync(CssFile, cssTemplate);
